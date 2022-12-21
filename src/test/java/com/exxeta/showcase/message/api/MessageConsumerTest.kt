0f8af18444efa2860e0e5402e78d8346d072ae40
Daniel Weber <daniel.weber@exxeta.com>
2022-12-21 17:54:45 +0100

    Adds tests for the message consumer

---
+++ src/test/java/com/exxeta/showcase/message/api/MessageConsumerTest.kt
@@ -0,0 +1,39 @@
+package com.exxeta.showcase.message.api
+
+import com.exxeta.showcase.message.MessageType
+import com.exxeta.showcase.message.control.MessageManager
+import com.exxeta.showcase.message.model.MessageRequestDto
+import io.mockk.every
+import io.mockk.verify
+import io.quarkiverse.test.junit.mockk.InjectMock
+import io.quarkus.test.junit.QuarkusTest
+import io.smallrye.mutiny.Uni
+import io.smallrye.mutiny.helpers.test.UniAssertSubscriber
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+
+@QuarkusTest
+@Suppress("ReactiveStreamsUnusedPublisher")
+internal class MessageConsumerTest {
+
+    @Inject
+    private lateinit var messageConsumer: MessageConsumer
+
+    @InjectMock
+    private lateinit var messageManager: MessageManager
+
+    @Test
+    fun consumeMessage() {
+        val dto = MessageRequestDto("Test content", MessageType.KAFKA)
+
+        val response = Uni.createFrom().nullItem<Void>()
+
+        every { messageManager.handleIncomingMessage(dto) } returns response
+
+        val subscriber = messageConsumer.consumeMessage(dto).subscribe().withSubscriber(UniAssertSubscriber.create())
+
+        verify { messageManager.handleIncomingMessage(dto) }
+
+        subscriber.assertCompleted()
+    }
+}
