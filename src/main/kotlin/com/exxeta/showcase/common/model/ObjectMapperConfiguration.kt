fe75496e0a8ad13db28f5133febaf3eb37018320
adbe <adrian.beilharz@exxeta.com>
2023-01-30 09:27:12 +0100

    add jackson datatype module for java.time and serialize Times as ISO String

--- src/main/kotlin/com/exxeta/showcase/common/model/ObjectMapperConfiguration.kt
+++ src/main/kotlin/com/exxeta/showcase/common/model/ObjectMapperConfiguration.kt
@@ -2,6 +2,7 @@ package com.exxeta.showcase.common.model
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.quarkus.jackson.ObjectMapperCustomizer
@@ -14,7 +15,8 @@ class ObjectMapperConfiguration {
     @Produces
     @Singleton
     fun provideObjectMapper(customizers: Instance<ObjectMapperCustomizer>): ObjectMapper {
-        return jacksonObjectMapper().registerModule(JavaTimeModule())
+        return jacksonObjectMapper()
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
             .enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
             .also { mapper ->
                 customizers.forEach { customizer ->
