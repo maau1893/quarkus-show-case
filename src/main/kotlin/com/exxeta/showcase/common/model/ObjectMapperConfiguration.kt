f2ccf6bc840f464e074e87e1acf8b4a987b6f4b5
Daniel Weber <daniel.weber@exxeta.com>
2023-01-30 10:18:29 +0100

    Remove import

--- src/main/kotlin/com/exxeta/showcase/common/model/ObjectMapperConfiguration.kt
+++ src/main/kotlin/com/exxeta/showcase/common/model/ObjectMapperConfiguration.kt
@@ -3,7 +3,6 @@ package com.exxeta.showcase.common.model
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.quarkus.jackson.ObjectMapperCustomizer
 import javax.enterprise.inject.Instance
